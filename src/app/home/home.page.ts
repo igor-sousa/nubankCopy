7565d5faec44f34b68f566b01ee1a0ff60ef48c0
Igor Sousa Do Nascimento <igor.nascimento@totvs.com.br>
2022-02-11 09:21:00 -0300

    feat: Criação da casca da homepage, apenas header e footer.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public menuopts: Array<any> = [
+    {icon: 'person-add-outline', text: 'Indicar amigos'},
+    {icon: 'phone-portrait-outline', text: 'Recarga de celular'},
+    {icon: 'wallet-outline', text: 'Depositar'},
+    {icon: 'options-outline', text: 'Ajustar limite'},
+    {icon: 'help-circle-outline', text: 'Me ajuda'},
+    {icon: 'barcode-outline', text: 'Pagar'},
+    {icon: 'lock-open-outline', text: 'Bloquear cartão'},
+    {icon: 'card-outline', text: 'Cartão virtual'},
+  ];
+
+  public slidesOpts: any = {slidesPerView: 3, freeMode: true};
+
   constructor() {}
 
 }
