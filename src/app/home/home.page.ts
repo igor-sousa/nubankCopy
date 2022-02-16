055a6e699c10fd503f084dd3c944ebebddfc665f
Igor Sousa Do Nascimento <igor.nascimento@totvs.com.br>
2022-02-16 09:08:42 -0300

    feat: Animação na transição da home para o menu do usuario.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { AnimationController, Animation, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+
+  @ViewChild('blocks', {read: ElementRef, static: true}) blocks: ElementRef;
+  @ViewChild('background', {read: ElementRef, static: true}) background: ElementRef;
 
   public menuopts: Array<any> = [
     {icon: 'person-add-outline', text: 'Indicar amigos'},
@@ -29,12 +33,48 @@ export class HomePage {
   ];
 
   public isHide = false;
+  public initialStep = 0;
 
-  constructor() {}
+  private maxTranslate: number;
+  private animation: Animation;
+
+  constructor(
+    private animationCtrl: AnimationController,
+    private platform: Platform,
+    private renderer: Renderer2
+  ) {
+    this.maxTranslate = this.platform.height() - 200;
+  }
+
+  ngAfterViewInit() {
+    this.animation = this.createAnimation();
+  }
+
+  createAnimation(): Animation {
+    const animation = this.animationCtrl.create()
+    .addElement(this.blocks.nativeElement)
+    .duration(300)
+    .fromTo('transform', 'translateY(0)', `translateY(${this.maxTranslate}px)`);
+
+    return animation;
+  }
+
+  setBackgroundOpacity() {
+    this.renderer.setStyle(this.background.nativeElement, 'opacity', this.initialStep === 0 ? '0' : '1');
+  }
+
+  toggleBlocks() {
+    const isInitialStep = this.initialStep === 0;
+    this.initialStep = isInitialStep ? this.maxTranslate : 0;
+
+    this.animation.direction(isInitialStep ? 'normal' : 'reverse');
+    this.animation.play();
+
+    this.setBackgroundOpacity();
+  }
 
   public hideValues(): void {
     this.isHide = !this.isHide;
-    console.log('botao foi clicado.');
   }
 
 }
