85071dbf063133233d878259d9e5e5b607ff2e0e
Igor Sousa Do Nascimento <igor.nascimento@totvs.com.br>
2022-02-18 09:49:51 -0300

    feat: Melhoria na transição da animação e adição de swipe.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
-import { AnimationController, Animation, Platform } from '@ionic/angular';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild, NgZone } from '@angular/core';
+import { AnimationController, Animation, Platform, Gesture, GestureController, GestureDetail } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,7 @@ export class HomePage implements AfterViewInit {
 
   @ViewChild('blocks', {read: ElementRef, static: true}) blocks: ElementRef;
   @ViewChild('background', {read: ElementRef, static: true}) background: ElementRef;
+  @ViewChild('swipeDown') swipeDown: any;
 
   public menuopts: Array<any> = [
     {icon: 'person-add-outline', text: 'Indicar amigos'},
@@ -34,43 +35,92 @@ export class HomePage implements AfterViewInit {
 
   public isHide = false;
   public initialStep = 0;
+  public swiping = false;
 
   private maxTranslate: number;
   private animation: Animation;
+  private gesture: Gesture;
 
   constructor(
     private animationCtrl: AnimationController,
     private platform: Platform,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private gestureCtrl: GestureController
   ) {
     this.maxTranslate = this.platform.height() - 200;
   }
 
   ngAfterViewInit() {
     this.animation = this.createAnimation();
+    this.detectSwipe();
   }
 
   createAnimation(): Animation {
     const animation = this.animationCtrl.create()
     .addElement(this.blocks.nativeElement)
     .duration(300)
-    .fromTo('transform', 'translateY(0)', `translateY(${this.maxTranslate}px)`);
+    .fromTo('transform', 'translateY(0)', `translateY(${this.maxTranslate}px)`)
+    .onFinish(() => this.gesture.enable(true));
 
     return animation;
   }
 
-  setBackgroundOpacity() {
-    this.renderer.setStyle(this.background.nativeElement, 'opacity', this.initialStep === 0 ? '0' : '1');
+  detectSwipe() {
+    this.gesture = this.gestureCtrl.create({
+      el: this.swipeDown.el,
+      gestureName: 'swipe-down',
+      threshold: 0,
+      onMove: ev => this.onMove(ev),
+      onEnd: ev => this.onEnd(ev)
+    }, true);
+
+    this.gesture.enable(true);
+  }
+
+  onMove(ev: GestureDetail) {
+    if (!this.swiping) {
+      this.animation.direction('normal').progressStart(true);
+      this.swiping = true;
+    }
+    const step = (this.initialStep + ev.deltaY) / this.maxTranslate;
+    this.animation.progressStep(step);
+    this.setBackgroundOpacity(step);
+  }
+
+  onEnd(ev: GestureDetail) {
+    if (!this.swiping) {
+      return;
+    } else {
+      this.gesture.enable(false);
+
+      const step = (this.initialStep + ev.deltaY) / this.maxTranslate;
+      const shouldComplete = step > 0.5;
+
+      this.animation.progressEnd(shouldComplete ? 1 : 0, step);
+      this.initialStep = shouldComplete ? this.maxTranslate : 0;
+      this.setBackgroundOpacity();
+      this.swiping = false;
+    }
+  }
+
+  setBackgroundOpacity(value: number = null) {
+    this.renderer.setStyle(this.background.nativeElement, 'opacity', value ? value : this.initialStep === 0 ? '0' : '1');
   }
 
   toggleBlocks() {
     const isInitialStep = this.initialStep === 0;
     this.initialStep = isInitialStep ? this.maxTranslate : 0;
 
+    this.gesture.enable(false);
+
     this.animation.direction(isInitialStep ? 'normal' : 'reverse');
     this.animation.play();
 
     this.setBackgroundOpacity();
+  }
+
+  fixedBlocks(): boolean {
+    return this.swiping || this.initialStep === this.maxTranslate;
   }
 
   public hideValues(): void {
